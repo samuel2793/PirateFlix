41b6aef082589af4688979e025778817dad27ba7
samuel2793 <padillasamuel2793@gmail.com>
2026-01-11 22:16:17 +0100

    feat: initialize Angular application structure with routing, styles, and testing setup
    - Added app routing configuration.
    - Created main application component with title signal.
    - Set up initial HTML structure in index.html.
    - Configured main entry point in main.ts for bootstrapping the application.
    - Established global styles in styles.scss.
    - Configured TypeScript settings in tsconfig files for app and spec.
    - Implemented unit tests for the app component.

---
+++ android/app/src/main/java/com/samuel2793/pirateflix/MainActivity.java
@@ -0,0 +1,5 @@
+package com.samuel2793.pirateflix;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.samuel2793.pirateflix',
+  appName: 'pirateflix',
+  webDir: 'dist/pirateflix/browser'
+};
+
+export default config;
